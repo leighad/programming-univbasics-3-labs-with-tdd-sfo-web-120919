a8bce6c6c194d8cafd44958f6fd01f09582250a6
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-05 20:52:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,9 @@ second_number = 27
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
+
+
+
+
+
